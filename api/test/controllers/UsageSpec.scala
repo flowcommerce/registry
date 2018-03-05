3db2bcc1f6c3ca72497000efe5c61cc531d58362
Matthew Mark Miller <m3@dasmb.com>
2018-03-05 17:45:34 -0500

    Test warning fix

--- api/test/controllers/UsageSpec.scala
+++ api/test/controllers/UsageSpec.scala
@@ -1,18 +1,17 @@
 package controllers
 
-import io.flow.play.util.AuthData
 import io.flow.test.utils.FlowPlaySpec
 import io.flow.usage.util.UsageUtil
 import io.flow.usage.v0.Client
 import io.flow.usage.v0.models.json._
-import org.scalatest.BeforeAndAfter
 import play.api.libs.json.Json
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class UsageSpec extends FlowPlaySpec {
-  def uu = app.injector.instanceOf[UsageUtil]
+  private[this] def uu = app.injector.instanceOf[UsageUtil]
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -23,8 +22,7 @@ class UsageSpec extends FlowPlaySpec {
       Await.result(
         new Client(
           wsClient,
-          s"http://localhost:$port",
-          defaultHeaders = authHeaders.headers(AuthData.Anonymous.Empty)
+          s"http://localhost:$port"
         ).Usages.getUsage(), 3 seconds
       )
     )
